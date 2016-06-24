f5fd823d294eccda26481c79affb3f584e216333
priyaraj-g <gpriyaraj13@gmail.com>
2016-06-24 09:59:35 +0530

    Create number.c

---
+++ number.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+   int n;
+   scanf("%d",&n);
+   if(n==0)
+   {
+       printf("zero");
+       
+   }
+   else if(n>0)
+   {
+       printf("positive");
+   }
+   else
+   {
+       printf("negative");
+   }
+    return 0;
+}
